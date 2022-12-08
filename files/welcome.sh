6fc1e9ddd6f06ecc08ec1d3ba94fd1a25d7a6ded
Angelo Machils <angelo@machils.org>
2022-12-08 13:35:06 +0200

    Some cleanup of the files.

--- files/welcome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat <<EOF >> /var/www/static-site/index.html
-<html>
-<body>
-<p>Hello, welcome to: $(hostname -f)</p>
-<p>My IP addresses are: $(hostname -i)</p>
-</body>
-</html>
-EOF
